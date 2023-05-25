5560a3062dc78ac3b73120efad1575adc01aa021
antoniocarlosdiniz8 <57872835+antoniocarlosdiniz8@users.noreply.github.com>
2023-05-24 21:38:18 -0300

    Update AulaJS008

--- AulaJS008/aula.js
+++ AulaJS008/aula.js
@@ -1,7 +1,14 @@
 
-var n1 = 1
+/*var n1 = 1
 var n2 = 3
 
 let soma = Number(n1) + Number(n2)
 
-console.log(`A soma entre ${n1} e ${n2} é : ${soma}`)
+console.log(`A soma entre ${n1} e ${n2} é : ${soma}`)*/
+
+numero = 5 + 3 / 2
+
+//console.log(10 == 10)
+//console.log(10 == '10')
+//console.log(10 === '10')
+console.log(numero)
