eb0ba636c6615a216ea366e0742a6e7c3a921b0d
antoniocarlosdiniz8 <57872835+antoniocarlosdiniz8@users.noreply.github.com>
2023-05-27 21:24:54 -0300

    Update AulaJS010

--- AulaJS008/aula.js
+++ AulaJS008/aula.js
@@ -1,8 +1,7 @@
-const somar = (num1, num2) => {
-    return num1 - num2
-  }
-  
-  // Exemplo de uso da função:
-  const resultado = somar(5, 3)
-  console.log(`O resultado é ${resultado}`)// Vai imprimir 8 no console
-  
+var a = 3
+var b = 5
+
+
+const sum = (a, b) => a + b
+
+console.log(sum)
