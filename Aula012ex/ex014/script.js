b57b4fd69f74b036df5c95ca61fd0851a95c6ec5
antoniocarlosdiniz8 <57872835+antoniocarlosdiniz8@users.noreply.github.com>
2023-05-29 09:35:30 -0300

    Update Ex014

--- Aula012ex/ex014/script.js
+++ Aula012ex/ex014/script.js
@@ -1,6 +1,31 @@
-var msg = document.getElementById('msg')
-var imagem = document.getElementById('foto')
+ function carregar(){
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
 
-data = new Date()
+    data = new Date()
 
-var hora data.getHours()
+    //var hora = data.getHours()
+
+    var hora = 19
+
+    msg.innerHTML = `Agora são ${hora} horas`
+
+    if (hora >= 0 && hora <= 12){
+        img.src = ''
+
+        document.body.style.background = '#97a5b0'  //97a5b0
+        //Bom dia
+
+    }else if(hora >= 12 && hora < 18){
+        img.src = './tarde.png'
+        document.body.style.background = '#f3c623'
+        //Boa Tarde
+    }else{
+        img.src = './noite.png'
+        document.body.style.background = '#213742'
+        //Boa Noite
+    }
+
+ }
+    
+
